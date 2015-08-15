8f6995ed63ac2da44f61ab9b14c85b7ae34a13dc
Hasan Yavuz ÖZDERYA <hy@ozderya.net>
2015-08-15 16:22:16 +0300

    remove accidentially committed test code

--- main.cpp
+++ main.cpp
@@ -51,33 +51,5 @@ int main(int argc, char *argv[])
     qDebug() << "Revision" << VERSION_REVISION;
 
     w.show();
-
-    // test framebuffer
-    FrameBuffer buffer(10);
-    QVector<double> s(3,15.);
-    s.append(1.0);
-    s.append(2.0);
-    s.append(3.0);
-    buffer.addSamples(s);
-    // buffer.addSamples(s);
-    // buffer.resize(5);
-    QwtPlot plot;
-    QwtPlotCurve curve;
-    curve.setSamples(&buffer);
-    curve.attach(&plot);
-    plot.show();
-    qDebug() << buffer.sample(0);
-    qDebug() << buffer.sample(1);
-    qDebug() << "size:" << buffer.size();
-    buffer.addSamples(s);
-    // curve.setSamples(&buffer);
-    plot.replot();
-    // qDebug() << buffer.sample(5);
-    // qDebug() << buffer.sample(7);
-    // qDebug() << buffer.sample(8);
-    // qDebug() << buffer.sample(9);
-    // qDebug() << buffer.sample(15);
-    // qDebug() << buffer.sample(19);
-
     return a.exec();
 }
