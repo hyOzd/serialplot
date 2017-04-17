4ddc4141f4fae629d89decf204ed8444830f1333
Hasan Yavuz ÖZDERYA <hy@ozderya.net>
2017-04-17 11:56:31 +0300

    removed configure step for version file

---
+++ src/version.h
@@ -0,0 +1,31 @@
+/*
+  Copyright © 2017 Hasan Yavuz Özderya
+
+  This file is part of serialplot.
+
+  serialplot is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  serialplot is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with serialplot.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef VERSION_H
+#define VERSION_H
+
+#ifndef VERSION_STRING
+#define VERSION_STRING   "0.8.1"
+#endif
+
+#ifndef VERSION_REVISION
+#define VERSION_REVISION ""
+#endif
+
+#endif // VERSION_H
